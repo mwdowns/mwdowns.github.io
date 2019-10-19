63265633283428adb9c736630563241a2cf0ae75
Matthew Downs <mwdowns@gmail.com>
2019-10-19 12:32:24 -0400

    being lazy, made one footer file

--- javascripts/footer.js
+++ javascripts/footer.js
@@ -1,11 +1,49 @@
-var footer = document
 
-footer.write(`
-<div class="footer">
-  <div class="navigation-links">
-    <a href="pages/about.html">about</a> | 
-    <a href="pages/projects.html">projects</a> | 
-    <a href="pages/scribbles.html">scribbles</a>
-  </div>
-</div>
-`)
+var footer = document
+var title = footer.title
+var about = "About - Matthew Downs"
+var projects = "Projects - Matthew Downs"
+var scribbles = "Scribbles - Matthew Downs"
+
+function write_footer() {
+  switch(title) {
+    case about:
+      return `
+        <div class="footer">
+          <div class="navigation-links">
+            <a href="../index.html">home</a> |
+            <a href="projects.html">projects</a> | 
+            <a href="scribbles.html">scribbles</a>
+          </div>
+        </div>`
+    case projects:
+      return `
+        <div class="footer">
+          <div class="navigation-links">
+            <a href="../index.html">home</a> |
+            <a href="about.html">about</a> | 
+            <a href="scribbles.html">scribbles</a>
+          </div>
+        </div>`
+    case(scribbles):
+      return `
+        <div class="footer">
+          <div class="navigation-links">
+            <a href="../index.html">home</a> |
+            <a href="about.html">about</a> | 
+            <a href="projects.html">projects</a>
+          </div>
+        </div>`
+    default:
+      return `
+        <div class="footer">
+          <div class="navigation-links">
+            <a href="pages/about.html">about</a> | 
+            <a href="pages/projects.html">projects</a> | 
+            <a href="pages/scribbles.html">scribbles</a>
+          </div>
+        </div>`
+  }
+}
+
+footer.write(write_footer())
